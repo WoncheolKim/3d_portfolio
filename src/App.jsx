1cb95e0ac342df734412eb5fff3a48a88526ac50
Woncheol Kim <icijiy@gmail.com>
2023-06-07 12:07:05 -0700

    Delete Tech

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,8 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        <Experience />
-        <Tech />
         <Works />
+        <Experience />
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
